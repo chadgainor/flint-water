4c2693ec80b224007558cb38432158940532c96f
Simeon Goolsby <goolsbysim@gmail.com>
2017-01-21 21:37:42 -0500

    Counter working

--- src/pages/counter/counter.ts
+++ src/pages/counter/counter.ts
@@ -5,48 +5,54 @@ import { Component } from '@angular/core';
   templateUrl: 'counter.html'
 })
 export class CounterPage {
-  milliseconds: number = 0;
+  //milliseconds: number = 0;
+  //seconds: number = 0;
   time_str: string;
   time: any;
   initial: string;
+  beginDate = new Date("April 25, 2014 12:00:00");
 
   ngOnInit() {
-        //this.milliseconds = parseInt(this.initial);
-        this.milliseconds = parseInt("000000");
-        this.time = this.convertTime(this.milliseconds);
+    let today = new Date();
+    let milliseconds = new Date().getTime() - this.beginDate.getTime();
+    let seconds = milliseconds / 1000;
+    this.time = this.convertTime(seconds);
   }
 
 
   constructor() {
     setInterval(()=>{
-              this.milliseconds = this.milliseconds + 1000;
-              this.time = this.convertTime(this.milliseconds);
+              let milliseconds = new Date().getTime() - this.beginDate.getTime();
+              let seconds = milliseconds / 1000;
+              this.time = this.convertTime(seconds);
     }, 1000);
   }
 
   /**
     * Converts milliseconds to 'hh:mm:ss' string
     *
-    * @param {number} milliseconds
+    * @param {number} seconds
     * @returns {string}
     */
-  convertTime (milliseconds:  number) {
-    let sec: number = milliseconds / 1000;
-    let hours, minutes, seconds: number;
-    let hours_str, minutes_str, seconds_str: string;
-    let h, m, s: string;
+  convertTime (seconds:  number) {
+    let days_str, hours_str, minutes_str, seconds_str: string;
+    //let d, h, m, s: string;
 
-    hours = Math.floor(((sec % 31536000) % 86400) / 3600);
-    hours_str = (hours <=9) ? '0' + hours.toString() : hours.toString() ;
+    let days = Math.floor(((seconds / 60) / 1440));
+    //let hours = Math.floor((seconds % 60) * 24);
+    let hours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+    //let minutes = Math.floor(hours * 60);
+    let minutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+    //let sec = Math.floor(minutes * 60);
+    let sec = Math.floor(((seconds % 31536000) % 86400) % 3600) % 60;
 
-    minutes = Math.floor((((sec % 31536000) % 86400) % 3600) / 60);
-    minutes_str = (minutes <=9) ? '0' + minutes.toString() : minutes.toString() ;
+    //seconds_str = (sec <=9) ? '0' + sec.toString() : sec.toString() ;
+    seconds_str = (sec <1) ? '0' : sec.toString() ;
+    minutes_str = (minutes <1) ? '0': minutes.toString() ;
+    hours_str = (hours <1) ? '0' : hours.toString() ;
+    days_str = (days <1) ? '0' : days.toString() ;
 
-    seconds = Math.floor(((sec % 31536000) % 86400) % 3600) % 60;
-    seconds_str = (seconds <=9) ? '0' + seconds.toString() : seconds.toString() ;
-
-    //return hours_str +':'+ minutes_str +':'+ seconds_str;
-    return {h: hours_str, m: minutes_str, s: seconds_str};
+    return {d: days_str, h: hours_str, m: minutes_str, s: seconds_str};
   }
 
 }
